1d625b5a959ce4338df5f06ac57cc1b824d55e7e
AaronRBetts <aaronrbetts@gmail.com>
2020-06-14 22:05:01 +0300

    Updates

---
+++ precache-manifest.6d5a3c9b5af23b00de6c8c4cd90d6b2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a98fba5f6497b5505d59694ccb2fe8d4",
+    "url": "/relay-timer/index.html"
+  },
+  {
+    "revision": "f8c6410403f0d06a6cd1",
+    "url": "/relay-timer/static/css/main.e0929319.chunk.css"
+  },
+  {
+    "revision": "15d0691d20684d8683e2",
+    "url": "/relay-timer/static/js/2.4e6b2970.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/relay-timer/static/js/2.4e6b2970.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8c6410403f0d06a6cd1",
+    "url": "/relay-timer/static/js/main.01c8a325.chunk.js"
+  },
+  {
+    "revision": "2dec303810cc7d70cd43",
+    "url": "/relay-timer/static/js/runtime-main.61461fc7.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/relay-timer/precache-manifest.6d5a3c9b5af23b00de6c8c4cd90d6b2a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/relay-timer/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
